64eac4b08631d61c8e6e98a5e4432b193e862b25
Arata Jingu <>
2019-02-19 01:01:21 +0900

    First Commit.

---
+++ Assets/Scripts/BezierCurve.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class BezierCurve
+{
+    static Vector3 QuadroBezierCurve(Vector3[] controllPoints, float t)
+    {
+        return controllPoints[0] * Mathf.Pow(1 - t, 2) +
+               controllPoints[1] * 2 * t * (1 - t) +
+               controllPoints[2] * Mathf.Pow(t, 2);
+    }
+
+    static Vector3 CubicBezierCurve(Vector3[] controllPoints, float t)
+    {
+        return controllPoints[0] * Mathf.Pow(1 - t, 3) +
+               controllPoints[1] * 3 * t * Mathf.Pow(1 - t, 2) +
+               controllPoints[2] * 3 * Mathf.Pow(t, 2) * (1 - t) +
+               controllPoints[3] * Mathf.Pow(t, 3);
+    }
+}
---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    private readonly Camera _targetCamera;
+
+    public CameraMover(Camera targetCamera)
+    {
+        _targetCamera = targetCamera;
+    }
+
+    public void JackIn()
+    {
+
+    }
+
+    public void JackOut()
+    {
+     
+    }
+}
