e5a8a95e01f30c7189d2d8330ab3c59c457edd2e
Arata Jingu <>
2019-02-19 11:05:39 +0900

    Implement motion

--- Assets/Scripts/BezierCurve.cs
+++ Assets/Scripts/BezierCurve.cs
@@ -2,18 +2,18 @@
 
 public static class BezierCurve
 {
-    static Vector3 QuadroBezierCurve(Vector3[] controllPoints, float t)
+    public static Vector3 QuadroBezierCurve(Vector3 p0, Vector3 p1, Vector3 p2, float t)
     {
-        return controllPoints[0] * Mathf.Pow(1 - t, 2) +
-               controllPoints[1] * 2 * t * (1 - t) +
-               controllPoints[2] * Mathf.Pow(t, 2);
+        return p0 * Mathf.Pow(1 - t, 2) +
+               p1 * 2 * t * (1 - t) +
+               p2 * Mathf.Pow(t, 2);
     }
 
-    static Vector3 CubicBezierCurve(Vector3[] controllPoints, float t)
+    public static Vector3 CubicBezierCurve(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
     {
-        return controllPoints[0] * Mathf.Pow(1 - t, 3) +
-               controllPoints[1] * 3 * t * Mathf.Pow(1 - t, 2) +
-               controllPoints[2] * 3 * Mathf.Pow(t, 2) * (1 - t) +
-               controllPoints[3] * Mathf.Pow(t, 3);
+        return p0 * Mathf.Pow(1 - t, 3) +
+               p1 * 3 * t * Mathf.Pow(1 - t, 2) +
+               p2 * 3 * Mathf.Pow(t, 2) * (1 - t) +
+               p3 * Mathf.Pow(t, 3);
     }
 }
